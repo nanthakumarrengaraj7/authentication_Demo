72db4f6a4a403ef09e8a9454e7c82c719224612b
nanthakumarrengaraj7 <nanthakumartyson777@gmail.com>
2025-11-05 21:49:34 +0530

    update delete user function

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { registerUser, loginUser, getAllUsers } = require('../controllers/userController');
+const { registerUser, loginUser, getAllUsers, deleteUser } = require('../controllers/userController');
 const auth = require('../middleware/auth');
 const router = express.Router();
 
 router.post('/register',registerUser);
 router.post('/login',loginUser);
-router.delete('/users/:id', deleteUser);
+router.delete('/users/:id',deleteUser);
 router.get('/users',getAllUsers);
 
 router.get('/profile',auth,(req,res)=>{
