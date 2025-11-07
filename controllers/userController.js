6efb8ffd572470a496776ba06af7b74919e453f9
nanthakumarrengaraj7 <nanthakumartyson777@gmail.com>
2025-11-07 19:03:46 +0530

    fix if condition

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,7 +29,7 @@ exports.loginUser = async (req, res) => {
         const { mail, password } = req.body;
 
         const existingUser = await userModel.findOne({ mail });
-        if (!user) {
+        if (!existingUser) {
             res.status(400).json({ message: 'User Not found' });
         }
 
